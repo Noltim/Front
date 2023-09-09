90200694c7e64ae81a40215a3a6d54977712ab50
José Milton de Oliveira Neto <milton_oliveira18@live.com>
2023-09-09 09:59:30 -0300

    Criação do form usando apenas css, html e JavaScript

---
+++ JavaScript1.js
@@ -0,0 +1,9 @@
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Obrigado! Vamos entrar em contato com a ' + nome.value + ' assim que possivel.');
+    }
+
+}
