9d7944640f47e0889bce8747a8d7f437e09c1907
José Milton de Oliveira Neto <milton_oliveira18@live.com>
2023-11-25 20:20:52 -0300

    Passo inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewForm from "./components/pages/NewForm";
+import Container from "./components/pages/layout/Container"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <Router>
+
+      <div>
+      <ul>
+        <Link to="/" >Home</Link>
+        <Link to="/contact" >Contato</Link>
+        <Link to="/company" >Empresa</Link>
+        <Link to="/newform" >Novo Formulario</Link>
+      </ul>
+      </div>
+
+      <Container>
+      <Routes>
+        <Route path="/" element= {< Home />}/>
+        <Route path="/company" element= {< Company />}/>
+        <Route path="/contact" element= {< Contact />}/>
+        <Route path="/newform" element= {< NewForm />}/>
+      </Routes>
+      </Container>
+
+      <footer>Rodapé</footer>
+
+    </Router>
   );
 }
 
---
+++ src/components/pages/NewForm.js
@@ -0,0 +1,5 @@
+function NewForm(){
+    return <h1> NewForm</h1>
+}
+
+export default NewForm
